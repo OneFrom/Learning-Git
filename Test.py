ac5eea0712bfa6ed30ddedfa993b7c78cafd4f81
OneFrom <onefrom@mail.ru>
2019-08-29 23:01:28 +0300

    version2

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
-#Знакомство с Python 2019-08-01
+#Знакомство с Python https://www.youtube.com/watch?v=7DU6ZGqbJFM&list=PLIcAMDxr6tpqJ3FHGVpYVn-puu6CJiOKh&index=2
 my_str = 'Тестовая строа с именем Иван'
 animals = ['Лев','Мышь','Кошка','Змея','Лось','Рыбка','Сова']
 members = 'James,Jonyy, Jvan, Jack, Jokoly'
