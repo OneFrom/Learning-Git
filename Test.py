133ade325aa67ce5e3d1209fdfe90a3680a90d30
OneFrom <onefrom@mail.ru>
2019-08-29 23:08:39 +0300

    version 3

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 #Знакомство с Python https://www.youtube.com/watch?v=7DU6ZGqbJFM&list=PLIcAMDxr6tpqJ3FHGVpYVn-puu6CJiOKh&index=2
-my_str = 'Тестовая строа с именем Иван'
+my_str = 'Тестовая строа с именем Иван ПРИВЕТ!!!'
 animals = ['Лев','Мышь','Кошка','Змея','Лось','Рыбка','Сова']
 members = 'James,Jonyy, Jvan, Jack, Jokoly'
 # join,replace,startswitch,endswitch, lower, upper, split - методы
