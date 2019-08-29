b7d2b766b8dc9790b0606a61ada26d15c2298e46
OneFrom <onefrom@mail.ru>
2019-08-29 22:57:21 +0300

    Test learning GIT

---
+++ Test.py
@@ -0,0 +1,9 @@
+#Знакомство с Python 2019-08-01
+my_str = 'Тестовая строа с именем Иван'
+animals = ['Лев','Мышь','Кошка','Змея','Лось','Рыбка','Сова']
+members = 'James,Jonyy, Jvan, Jack, Jokoly'
+# join,replace,startswitch,endswitch, lower, upper, split - методы
+# min, max, abs, sum - функции для работы  с числами
+
+# создание списка
+print(members.split(','))
